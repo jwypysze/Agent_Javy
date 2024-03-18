5b9d965ed39dd4958b1798faf7b2821937e94550
jwypysze <joanna.wypyszewska@gmail.com>
2024-03-18 18:55:00 +0100

    Agent Javy poprawki

---
+++ src/main/java/com/kodilla/Agent_Javy/AgentJavyApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.Agent_Javy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentJavyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentJavyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kodilla/Agent_Javy/User.java
@@ -0,0 +1,8 @@
+package com.kodilla.Agent_Javy;
+
+public class User {
+
+    public void doSomething() {
+        System.out.println("Do something");
+    }
+}
